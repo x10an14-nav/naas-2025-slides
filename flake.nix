14a46c719ec1bba73a9530cf13e738de41c35d33
@x10an14-nav <christian.chavez@nav.no>
2025-10-13 16:34:06 +0200

    Add initial set of tooling

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+  outputs =
+    inputs:
+    let
+      inherit (inputs.nixpkgs) lib;
+      # Small tool to iterate over each systems
+      eachSystem =
+        f: lib.genAttrs [ "x86_64-linux" ] (system: f (import inputs.nixpkgs { inherit system; }));
+      treefmtEval = eachSystem (pkgs: inputs.treefmt-nix.lib.evalModule pkgs { });
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.marp-cli ];
+        };
+      });
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+    };
+}
