5459884d5cce2afbe5d2fee21a4a617f3b80a340
x10an14-nav <christian.chavez@nav.no>
2025-10-15 22:44:55 +0200

    wip: legg til ikke-fungerende nix flake snarveier

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
       eachSystem =
         f: lib.genAttrs [ "x86_64-linux" ] (system: f (import inputs.nixpkgs { inherit system; }));
       treefmtEval = eachSystem (pkgs: inputs.treefmt-nix.lib.evalModule pkgs { });
+      repoRoot = ./.;
     in
     {
       devShells = eachSystem (pkgs: {
@@ -21,6 +22,47 @@
             pdfpc
           ];
         };
+      });
+      apps = eachSystem (pkgs: rec {
+        default = pdf;
+        html = {
+          type = "app";
+          program = "xdg-open ${inputs.self.packages.${pkgs.system}.html}";
+        };
+        pdf = {
+          type = "app";
+          program = "${lib.getExe pkgs.pdfpc} ${inputs.self.packages.${pkgs.system}.pdf}";
+        };
+      });
+      packages = eachSystem (pkgs: rec {
+        default = pdf;
+        html =
+          pkgs.runCommand "buildHTML"
+            {
+              buildInputs = with pkgs; [
+                marp-cli
+                firefox
+              ];
+            }
+            # Bash
+            ''marp --output $out ${repoRoot}/slides.md'';
+        pdf =
+          pkgs.runCommand "buildPDF"
+            {
+              buildInputs = with pkgs; [
+                marp-cli
+                firefox
+              ];
+            }
+            (
+              lib.concatStringsSep " " [
+                "marp"
+                "--debug=all"
+                "--pdf-notes"
+                "--pdf-outlines"
+                "${repoRoot}/slides.md"
+              ]
+            );
       });
       formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
     };
